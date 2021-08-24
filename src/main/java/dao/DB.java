83f1de31756ae897f6737c785961c2c9c7d66c64
Naphtali-cpu <naphtali919@gmail.com>
2021-08-24 14:28:18 +0300

    Database deployment

--- src/main/java/dao/DB.java
+++ src/main/java/dao/DB.java
@@ -1,7 +1,9 @@
 package dao;
-
 import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/newsportal", "naphtali", "1606525");
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/newsportal", "naphtali", "1606525");
+
+    public static String connectionString = "jdbc:postgresql://ec2-52-6-77-239.compute-1.amazonaws.com:5432/d8lnbh95hoj830"; //!
+    public static Sql2o sql2o = new Sql2o(connectionString, "qwifhkajqelgfi", "e092647dacb35df68b5f84475c01b6ec0c6de819d57d4eaa33ca02dcd91cc5e3");
 }
