517b9bbadbcc14493597f4d3de4f4b4d3e592b03
Naphtali-cpu <naphtali919@gmail.com>
2021-08-24 21:14:49 +0300

    Added an exception

--- src/main/java/exceptions/ApiException.java
+++ src/main/java/exceptions/ApiException.java
@@ -1,5 +1,7 @@
 package exceptions;
 
+import java.util.Scanner;
+
 public class ApiException extends RuntimeException{
     private final int statusCode;
 
@@ -11,5 +13,17 @@ public class ApiException extends RuntimeException{
     public int getStatusCode() {
         return statusCode;
     }
+        public static void main(String[] args) {
+            Scanner kb = new Scanner(System.in);
+            System.out.println("Enter a number");
+            try {
+                long nb1 = kb.nextLong();
+                if(nb1<1000000000)
+                    throw new ArithmeticException();
+                else System.out.println( "result : " + Math.sqrt(nb1) );
+            } catch (ArithmeticException e) {
+                System.out.println("This is an arithmetic exception!!");
+            }
+        }
 
 }
